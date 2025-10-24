f6bfd4e5f08d24eb97e356bada84dd7cf075d6ac
Aryan Barde <147256003+aryanbarde80@users.noreply.github.com>
2025-10-24 18:07:23 +0530

    Update firebase-config.js

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -1,19 +1,21 @@
-// Firebase configuration for client-side authentication
-// This should be your web app config from Firebase Console > Project Settings > General > Web apps
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCFvbQU6h5-18NMKtWjgpCCoPWxpi76pBk",
-  authDomain: "pharmacy-managment-4b55f.firebaseapp.com",
-  projectId: "pharmacy-managment-4b55f",
-  storageBucket: "pharmacy-managment-4b55f.appspot.com",
-  messagingSenderId: "473321404811",
-  appId: "1:473321404811:web:4707ce0075e069a80ad78e",
-  measurementId: "G-9EESYMDYQS"
+  apiKey: "AIzaSyDwH8jipZvnVneqDCi7xsroH6b8d7uBhJ8",
+  authDomain: "pharma-e06d0.firebaseapp.com",
+  projectId: "pharma-e06d0",
+  storageBucket: "pharma-e06d0.firebasestorage.app",
+  messagingSenderId: "276212911327",
+  appId: "1:276212911327:web:0ef0b0682ddb7197a1ad6a",
+  measurementId: "G-DVTJ0VM3TK"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-
-// Export for use in other files
-window.firebaseAuth = auth;
-window.firebaseConfig = firebaseConfig;
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
